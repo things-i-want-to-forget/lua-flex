2541675be9ac5762d1d73f4550b44ee294b68d6b
LennyPenny <l.bernhardt@live.de>
2015-05-20 00:07:43 +0200

    added usercmd:KeyDown

--- lua/init.lua
+++ lua/init.lua
@@ -4,6 +4,7 @@ include "extensions/in_buttons.lau";
 include "extensions/vector.lau";
 include "extensions/angle.lau";
 include "extensions/entity.lau";
+include "extensions/usercmd.lau";
 
 include"scripts/loader.lau"
 
