2d48385476e7e171e3d19b9b9fbc36a55682fafc
LennyPenny <l.bernhardt@live.de>
2015-05-26 23:50:22 +0200

    let's just have one enum file

--- lua/init.lua
+++ lua/init.lua
@@ -1,6 +1,6 @@
 include "hook/hook.lau";
 
-include "extensions/in_buttons.lau";
+include "extensions/enums.lau";
 include "extensions/vector.lau";
 include "extensions/angle.lau";
 include "extensions/entity.lau";
